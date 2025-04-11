6d5d6b46dc182381be1173a01ddd7a9ea7f65880
stoneKong0810 <stonekong2115@gmail.com>
2025-04-11 21:38:51 +0900

    Design: 캐러셀 디자인 변경

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,18 +1,18 @@
 import './Carousel.css';
 
-import AutoScroll from 'embla-carousel-auto-scroll';
+import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
 
 const Carousel: React.FC = () => {
   const [emblaRef] = useEmblaCarousel({ loop: true }, [
-    AutoScroll({
+    Autoplay({
       stopOnInteraction: false,
-      speed: 2,
-      startDelay: 300,
       stopOnMouseEnter: true,
+      delay: 3000,
     }),
   ]);
 
+  // 캐러셀 이미지 가운데 정렬 및 스크롤 되려면 이미지 최소 6개 필요
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
@@ -43,6 +43,12 @@ const Carousel: React.FC = () => {
         <div className="embla__slide">
           <img
             src="https://cdn.pixabay.com/photo/2022/05/22/16/49/outdoors-7213951_640.jpg"
+            alt="carousel"
+          />
+        </div>
+        <div className="embla__slide">
+          <img
+            src="https://cdn.pixabay.com/photo/2025/04/08/10/42/landscape-9521261_1280.jpg"
             alt="carousel"
           />
         </div>
