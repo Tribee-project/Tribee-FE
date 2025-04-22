89c9e450821914973a98e3c90eeb74f896574e6c
stoneKong0810 <stonekong2115@gmail.com>
2025-04-22 20:33:05 +0900

    Refactor: 캐러셀 컴포넌트 리팩토링

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,53 +4,39 @@ import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
 
 const Carousel: React.FC = () => {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [
-    Autoplay({
-      stopOnInteraction: false,
-      delay: 3000,
-    }),
-  ]);
+  const [emblaRef] = useEmblaCarousel(
+    {
+      loop: true,
+      align: 'center',
+      containScroll: false,
+      slidesToScroll: 1,
+      inViewThreshold: 0,
+    },
+    [
+      Autoplay({
+        stopOnInteraction: false,
+        delay: 3000,
+      }),
+    ],
+  );
 
-  // 캐러셀 이미지 가운데 정렬 및 스크롤 되려면 이미지 최소 6개 필요
+  const carouselImages = [
+    'https://scdn.ybtour.co.kr/banner/common/202504081233057291306001001040.jpg',
+    'https://scdn.ybtour.co.kr/banner/common/202503250347279602308056001039.jpg',
+    'https://scdn.ybtour.co.kr/banner/common/202504150406434572301012001014.jpg',
+    'https://scdn.ybtour.co.kr/banner/common/202503210729472292308056001094.jpg',
+    'https://scdn.ybtour.co.kr/banner/common/202504010503251942301012001065.jpg',
+    'https://scdn.ybtour.co.kr/banner/common/202504110909166242308056001025.jpg',
+  ];
+
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
-        <div className="embla__slide">
-          <img
-            src="https://scdn.ybtour.co.kr/banner/common/202504081233057291306001001040.jpg"
-            alt="carousel"
-          />
-        </div>
-        <div className="embla__slide">
-          <img
-            src="https://scdn.ybtour.co.kr/banner/common/202503250347279602308056001039.jpg"
-            alt="carousel"
-          />
-        </div>
-        <div className="embla__slide">
-          <img
-            src="https://scdn.ybtour.co.kr/banner/common/202504150406434572301012001014.jpg"
-            alt="carousel"
-          />
-        </div>
-        <div className="embla__slide">
-          <img
-            src="https://scdn.ybtour.co.kr/banner/common/202503210729472292308056001094.jpg"
-            alt="carousel"
-          />
-        </div>
-        <div className="embla__slide">
-          <img
-            src="https://scdn.ybtour.co.kr/banner/common/202504010503251942301012001065.jpg"
-            alt="carousel"
-          />
-        </div>
-        <div className="embla__slide">
-          <img
-            src="https://scdn.ybtour.co.kr/banner/common/202504110909166242308056001025.jpg"
-            alt="carousel"
-          />
-        </div>
+        {carouselImages.map((imageUrl, index) => (
+          <div className="embla__slide" key={index}>
+            <img src={imageUrl} alt={`carousel-${index + 1}`} />
+          </div>
+        ))}
       </div>
     </div>
   );
