b363ca1f9438483b4c9a18f9d9779d2e7c779aa2
stoneKong0810 <stonekong2115@gmail.com>
2025-04-27 16:24:51 +0900

    Refactor: 페이지 이동 로직 변경

--- src/components/UserInfoButtons/UserInfoButtons.tsx
+++ src/components/UserInfoButtons/UserInfoButtons.tsx
@@ -1,13 +1,28 @@
 import { AliwangwangOutlined, BookOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 
+import useAuth from '@/hooks/useAuth';
+
 const UserInfoButtons: React.FC = () => {
+  const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
+
+  const handleUserInfo = () => {
+    if (isAuthenticated()) {
+      navigate('/user/info');
+    }
+  };
+
+  const handleUserBooked = () => {
+    if (isAuthenticated()) {
+      navigate('/user/booked');
+    }
+  };
 
   return (
     <div className="flex items-center gap-6 text-4xl">
-      <AliwangwangOutlined onClick={() => navigate('/user/info')} />
-      <BookOutlined onClick={() => navigate('/user/booked')} />
+      <AliwangwangOutlined onClick={handleUserInfo} />
+      <BookOutlined onClick={handleUserBooked} />
     </div>
   );
 };
