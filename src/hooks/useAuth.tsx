9c86001f17d1b4b898e19b563a0d428b04e01e4c
stoneKong0810 <stonekong2115@gmail.com>
2025-04-27 16:11:49 +0900

    Fix: 컴포넌트 마운트시 useEffect 부분에서 alert 두번 표시되는 문제 해결

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,11 +1,14 @@
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const useAuth = () => {
   const navigate = useNavigate();
+  const alertShown = useRef(false);
 
   const isAuthenticated = () => {
     const token = localStorage.getItem('accessToken');
-    if (!token) {
+    if (!token && !alertShown.current) {
+      alertShown.current = true;
       alert('로그인이 필요한 페이지입니다.');
       navigate('/login');
       return false;
