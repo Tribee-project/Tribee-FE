f32e97dd5677f244e00614fb593df6566ac0d505
stoneKong0810 <stonekong2115@gmail.com>
2025-04-27 16:17:24 +0900

    Refactor: Strict 모드 제거

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,14 +1,11 @@
-import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const useAuth = () => {
   const navigate = useNavigate();
-  const alertShown = useRef(false);
 
   const isAuthenticated = () => {
     const token = localStorage.getItem('accessToken');
-    if (!token && !alertShown.current) {
-      alertShown.current = true;
+    if (!token) {
       alert('로그인이 필요한 페이지입니다.');
       navigate('/login');
       return false;
