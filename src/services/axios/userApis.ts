501a40c5df9316fdd5af622fe5f714421ab40301
stonekong0810 <stonekong2115@gmail.com>
2025-05-30 00:36:32 +0900

    Feat: 예약 정보 조회 기능 추가 및 API 경로 수정

--- src/services/axios/userApis.ts
+++ src/services/axios/userApis.ts
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
+interface UserBooked {
+  id: string;
+  userId: string;
+  prodId: string;
+  reservationDate: string;
+  departureDate: string;
+  cost: number;
+  personnel: number;
+  status: number;
+}
+
 const userApi = axios.create({
-  baseURL: 'https://cfb9-125-133-70-87.ngrok-free.app/api/v1/user',
+  baseURL: 'https://cfb9-125-133-70-87.ngrok-free.app/api/v1',
   headers: {
     'ngrok-skip-browser-warning': '69420',
   },
@@ -21,16 +32,21 @@ userApi.interceptors.request.use(
 );
 
 const getUserInfo = async () => {
-  const response = await userApi.get('/info');
+  const response = await userApi.get('/user/info');
   return response.data;
 };
 
 const editUserNickname = async (data: { nickname: string }): Promise<void> => {
-  await userApi.put('/info', data);
+  await userApi.put('/user/info', data);
 };
 
 const editUserPassword = async (data: { password: string }): Promise<void> => {
-  await userApi.put('/password', data);
+  await userApi.put('/user/password', data);
 };
 
-export { editUserNickname, editUserPassword, getUserInfo };
+const getUserBooked = async (): Promise<UserBooked[]> => {
+  const response = await userApi.get('/reservation');
+  return response.data;
+};
+
+export { editUserNickname, editUserPassword, getUserBooked, getUserInfo };
