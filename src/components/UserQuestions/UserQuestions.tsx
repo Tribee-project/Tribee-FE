2cec0451c6b27c2efc58939dac007949e7f5e45d
stoneKong0810 <stonekong2115@gmail.com>
2025-05-31 17:24:20 +0900

    Feat: UserQuestions 컴포넌트 추가 및 질문 목록을 Collapse로 표시하는 기능 구현

--- src/components/UserQuestions/UserQuestions.tsx
+++ src/components/UserQuestions/UserQuestions.tsx
@@ -1,5 +1,32 @@
+import { Collapse, CollapseProps, ConfigProvider } from 'antd';
+
+import questionList from '@/constant/questionList';
+
+const items: CollapseProps['items'] = questionList.map((question) => ({
+  key: question.key,
+  label: question.label,
+  children: <p>{question.children}</p>,
+}));
+
 const UserQuestions: React.FC = () => {
-  return <div>UserQuestions</div>;
+  return (
+    <div className="mt-10 mb-10 w-250 self-center">
+      <ConfigProvider
+        theme={{
+          token: {
+            borderRadius: 5,
+          },
+          components: {
+            Collapse: {
+              contentBg: '#FEF3D0',
+            },
+          },
+        }}
+      >
+        <Collapse accordion size="large" items={items} />
+      </ConfigProvider>
+    </div>
+  );
 };
 
 export default UserQuestions;
---
+++ src/constant/questionList.ts
@@ -0,0 +1,24 @@
+const questionList = [
+  {
+    key: '1',
+    label: '예약 취소 및 변경은 어떻게 해야하나요?',
+    children: '예약 취소 및 변경은 예약 페이지에서 할 수 있습니다.',
+  },
+  {
+    key: '2',
+    label: '환불 규정은 어떻게 되나요?',
+    children: '환불 규정은 환불 페이지에서 확인할 수 있습니다.',
+  },
+  {
+    key: '3',
+    label: '트리비 마스코트는왜 꿀벌인가요?',
+    children: '트리비 마스코트는 꿀벌입니다.',
+  },
+  {
+    key: '4',
+    label: '여행 일정이 변경될 수도 있나요?',
+    children: '여행 일정이 변경될 수도 있습니다.',
+  },
+];
+
+export default questionList;
