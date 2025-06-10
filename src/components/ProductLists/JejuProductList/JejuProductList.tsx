e01ef832afc1ed4040e957c95a44b82861ed12bf
stonekong0810 <stonekong2115@gmail.com>
2025-06-10 22:05:59 +0900

    Feat: 제주도 상품 목록에 출발 일정 선택 기능 및 UI 개선

--- src/components/ProductLists/JejuProductList/JejuProductList.tsx
+++ src/components/ProductLists/JejuProductList/JejuProductList.tsx
@@ -1,5 +1,11 @@
-import { notification, Space } from 'antd';
+import 'dayjs/locale/ko';
+
+import { ConfigProvider, DatePicker, notification, Space } from 'antd';
+import locale from 'antd/locale/ko_KR';
+import dayjs from 'dayjs';
 import { useState } from 'react';
+
+dayjs.locale('ko');
 
 const TRAVEL_DAYS = [
   {
@@ -55,23 +61,50 @@ const JejuProductList: React.FC = () => {
   return (
     <div className="mt-10 mb-10 flex justify-center gap-15">
       {contextHolder}
-      <div className="flex w-40 flex-col gap-1">
-        <div>여행 기간</div>
-        <div className="flex flex-wrap gap-2">
-          {TRAVEL_DAYS.map((day) => (
-            <button
-              key={day.value}
-              className={`cursor-pointer rounded-md border-1 border-amber-300 px-3 py-1 text-sm ${
-                selectedDay === day.value ? 'bg-amber-300' : 'bg-white'
-              }`}
-              onClick={() => handleDayClick(day.value)}
-            >
-              {day.label}
-            </button>
-          ))}
+      <div className="flex w-40 flex-col gap-15">
+        <div className="flex flex-col gap-2">
+          <div className="text-sm font-bold">여행 기간</div>
+          <div className="flex flex-wrap gap-2">
+            {TRAVEL_DAYS.map((day) => (
+              <button
+                key={day.value}
+                className={`cursor-pointer rounded-md border-1 border-amber-300 px-3 py-1 text-sm ${
+                  selectedDay === day.value ? 'bg-amber-300' : 'bg-white'
+                }`}
+                onClick={() => handleDayClick(day.value)}
+              >
+                {day.label}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="text-sm font-bold">출발 일정</div>
+          <ConfigProvider
+            locale={locale}
+            theme={{
+              components: {
+                DatePicker: {
+                  colorBorder: '#FECA3A',
+                  hoverBorderColor: '#FECA3A',
+                  activeBorderColor: '#FECA3A',
+                },
+              },
+            }}
+          >
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <DatePicker
+                onChange={() => {}}
+                picker="month"
+                placeholder="출발 월"
+                size="middle"
+                style={{ width: '100%' }}
+              />
+            </Space>
+          </ConfigProvider>
         </div>
       </div>
-      <div className="flex w-200 flex-col items-center">
+      <div className="flex w-200 flex-col items-center gap-10">
         <Space>
           <div
             className="w-200 cursor-pointer bg-gray-200 p-2 text-center"
@@ -80,6 +113,26 @@ const JejuProductList: React.FC = () => {
             <p>🍊 제주도 여행시 안내사항을 확인하세요</p>
           </div>
         </Space>
+        <div className="flex w-full flex-col gap-5">
+          <div className="flex">
+            <div className="h-50 w-50 bg-gray-300">이미지</div>
+            <div className="flex w-150 flex-col bg-gray-200 p-7">
+              <p className="overflow-hidden text-lg text-ellipsis whitespace-nowrap">
+                4박 5일 제주도 감성여행 패키지
+                #우도#렌트카#제주동부#성산일출봉#성산맛집
+              </p>
+              <div className="mt-auto flex items-end justify-between gap-2">
+                <div className="flex gap-2">
+                  <p>여행 일정 | </p>
+                  <p>2025.06.10 ~ 2025.06.14</p>
+                </div>
+                <div className="text-lg font-bold">
+                  <p>399,000원 ~</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
