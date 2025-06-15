568ca80f54400e8d5135c69100d82a4bd63937d1
stoneKong0810 <stonekong2115@gmail.com>
2025-06-15 23:19:39 +0900

    Refactor: App 컴포넌트에서 라우팅 로직을 별도의 AppRoutes 컴포넌트로 분리하여 코드 가독성 향상

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
-
-import LoginPage from '@/pages/LoginPage/LoginPage';
-import MainPage from '@/pages/MainPage/MainPage';
-import SignupCompletePage from '@/pages/SignupCompletePage/SignupCompletePage';
-import SignupPage from '@/pages/SignupPage/SignupPage';
-import UserPage from '@/pages/UserPage/UserPage';
-
-import AmericasProductList from './components/ProductLists/AmericasProductList/AmericasProductList';
-import AsiaProductList from './components/ProductLists/AsiaProductList/AsiaProductList';
-import AustraliaProductList from './components/ProductLists/AustraliaProductList/AustraliaProductList';
-import ChinaProductList from './components/ProductLists/ChinaProductList/ChinaProductList';
-import DomesticProductList from './components/ProductLists/DomesticProductList/DomesticProductList';
-import EuropeProductList from './components/ProductLists/EuropeProductList/EuropeProductList';
-import EventProductList from './components/ProductLists/EventProductList/EventProductList';
-import HoneymoonProductList from './components/ProductLists/HoneymoonProductList/HoneymoonProductList';
-import JapanProductList from './components/ProductLists/JapanProductList/JapanProductList';
-import JejuProductList from './components/ProductLists/JejuProductList/JejuProductList';
-import TourProductList from './components/ProductLists/TourProductList/TourProductList';
-import UsProductList from './components/ProductLists/UsProductList/UsProductList';
-import WorkshopProductList from './components/ProductLists/WorkshopProductList/WorkshopProductList';
-import UserBooked from './components/UserBooked/UserBooked';
-import UserInfo from './components/UserInfo/UserInfo';
-import UserQuestions from './components/UserQuestions/UserQuestions';
-import UserReviews from './components/UserReviews/UserReviews';
-import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage';
-import ProductListPage from './pages/ProductListPage/ProductListPage';
+import AppRoutes from './routes';
 
 const App: React.FC = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup">
-        <Route path="email" element={<SignupPage />} />
-        <Route path="validate-email" element={<SignupPage />} />
-        <Route path="password" element={<SignupPage />} />
-        <Route path="nickname" element={<SignupPage />} />
-      </Route>
-      <Route path="/signup-complete" element={<SignupCompletePage />} />
-      <Route path="/user" element={<UserPage />}>
-        <Route path="info" element={<UserInfo />} />
-        <Route path="booked" element={<UserBooked />} />
-        <Route path="reviews" element={<UserReviews />} />
-        <Route path="questions" element={<UserQuestions />} />
-      </Route>
-      <Route path="/product-list" element={<ProductListPage />}>
-        <Route path="jeju" element={<JejuProductList />} />
-        <Route path="domestic" element={<DomesticProductList />} />
-        <Route path="europe" element={<EuropeProductList />} />
-        <Route path="asia" element={<AsiaProductList />} />
-        <Route path="china" element={<ChinaProductList />} />
-        <Route path="japan" element={<JapanProductList />} />
-        <Route path="americas" element={<AmericasProductList />} />
-        <Route path="australia" element={<AustraliaProductList />} />
-        <Route path="us" element={<UsProductList />} />
-        <Route path="tour" element={<TourProductList />} />
-        <Route path="honeymoon" element={<HoneymoonProductList />} />
-        <Route path="workshop" element={<WorkshopProductList />} />
-        <Route path="event" element={<EventProductList />} />
-      </Route>
-      <Route path="/product-detail" element={<ProductDetailPage />} />
-    </Routes>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,149 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import UserBooked from '@/components/UserBooked/UserBooked';
+import UserInfo from '@/components/UserInfo/UserInfo';
+import UserQuestions from '@/components/UserQuestions/UserQuestions';
+import UserReviews from '@/components/UserReviews/UserReviews';
+import LoginPage from '@/pages/LoginPage/LoginPage';
+import MainPage from '@/pages/MainPage/MainPage';
+import ProductListPage from '@/pages/ProductListPage/ProductListPage';
+import SignupCompletePage from '@/pages/SignupCompletePage/SignupCompletePage';
+import SignupPage from '@/pages/SignupPage/SignupPage';
+import UserPage from '@/pages/UserPage/UserPage';
+
+const JejuProductList = lazy(
+  () => import('@/components/ProductLists/JejuProductList/JejuProductList'),
+);
+const DomesticProductList = lazy(
+  () =>
+    import('@/components/ProductLists/DomesticProductList/DomesticProductList'),
+);
+const EuropeProductList = lazy(
+  () => import('@/components/ProductLists/EuropeProductList/EuropeProductList'),
+);
+const AsiaProductList = lazy(
+  () => import('@/components/ProductLists/AsiaProductList/AsiaProductList'),
+);
+const ChinaProductList = lazy(
+  () => import('@/components/ProductLists/ChinaProductList/ChinaProductList'),
+);
+const JapanProductList = lazy(
+  () => import('@/components/ProductLists/JapanProductList/JapanProductList'),
+);
+const AmericasProductList = lazy(
+  () =>
+    import('@/components/ProductLists/AmericasProductList/AmericasProductList'),
+);
+const AustraliaProductList = lazy(
+  () =>
+    import(
+      '@/components/ProductLists/AustraliaProductList/AustraliaProductList'
+    ),
+);
+const UsProductList = lazy(
+  () => import('@/components/ProductLists/UsProductList/UsProductList'),
+);
+const TourProductList = lazy(
+  () => import('@/components/ProductLists/TourProductList/TourProductList'),
+);
+const HoneymoonProductList = lazy(
+  () =>
+    import(
+      '@/components/ProductLists/HoneymoonProductList/HoneymoonProductList'
+    ),
+);
+const WorkshopProductList = lazy(
+  () =>
+    import('@/components/ProductLists/WorkshopProductList/WorkshopProductList'),
+);
+const EventProductList = lazy(
+  () => import('@/components/ProductLists/EventProductList/EventProductList'),
+);
+
+const ProductDetailPage = lazy(
+  () => import('@/pages/ProductDetailPage/ProductDetailPage'),
+);
+
+const LoadingSpinner = () => (
+  <div className="flex min-h-96 items-center justify-center">
+    <div className="h-12 w-12 animate-spin rounded-full border-b-2 border-amber-500"></div>
+  </div>
+);
+
+const signupRoutes = ['email', 'validate-email', 'password', 'nickname'];
+
+const userRoutes = [
+  { path: 'info', component: UserInfo },
+  { path: 'booked', component: UserBooked },
+  { path: 'reviews', component: UserReviews },
+  { path: 'questions', component: UserQuestions },
+];
+
+const productListRoutes = [
+  { path: 'jeju', component: JejuProductList },
+  { path: 'domestic', component: DomesticProductList },
+  { path: 'europe', component: EuropeProductList },
+  { path: 'asia', component: AsiaProductList },
+  { path: 'china', component: ChinaProductList },
+  { path: 'japan', component: JapanProductList },
+  { path: 'americas', component: AmericasProductList },
+  { path: 'australia', component: AustraliaProductList },
+  { path: 'us', component: UsProductList },
+  { path: 'tour', component: TourProductList },
+  { path: 'honeymoon', component: HoneymoonProductList },
+  { path: 'workshop', component: WorkshopProductList },
+  { path: 'event', component: EventProductList },
+];
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      {/* 메인 페이지 */}
+      <Route path="/" element={<MainPage />} />
+
+      {/* 인증 관련 라우트 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup">
+        {signupRoutes.map((route) => (
+          <Route key={route} path={route} element={<SignupPage />} />
+        ))}
+      </Route>
+      <Route path="/signup-complete" element={<SignupCompletePage />} />
+
+      {/* 사용자 페이지 */}
+      <Route path="/user" element={<UserPage />}>
+        {userRoutes.map(({ path, component: Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+      </Route>
+
+      {/* 상품 목록 페이지 */}
+      <Route path="/product-list" element={<ProductListPage />}>
+        {productListRoutes.map(({ path, component: Component }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<LoadingSpinner />}>
+                <Component />
+              </Suspense>
+            }
+          />
+        ))}
+      </Route>
+
+      {/* 상품 상세 페이지 */}
+      <Route
+        path="/product-detail"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <ProductDetailPage />
+          </Suspense>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
